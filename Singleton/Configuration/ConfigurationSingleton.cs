7b15452b86bc4b08aac8d5832eebbfac929c668e
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2024-01-22 14:50:20 +0100

    Design pattern Singleton

---
+++ Singleton/Configuration/ConfigurationSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Configuration
+{
+	public class ConfigurationSingleton
+	{
+        private static ConfigurationSingleton instance = null;
+		private static object objToLock = new object();
+
+        public string StringProperty { get; set; }
+		public int IntProperty { get; set; }
+
+        private ConfigurationSingleton() { }
+		public static ConfigurationSingleton GetInstance()
+		{
+			lock(objToLock) //multithreading protection
+			{
+				if(instance == null)
+				{
+					instance = new ConfigurationSingleton();
+				}
+			}
+			return instance;
+		}
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Singleton.Configuration;
+
+Console.WriteLine("-------------------------------------------------------------------");
+
+//var cfg2 = new ConfigurationSingleton(); //error, constructor is private
+var cfg = ConfigurationSingleton.GetInstance();
+var cfg2 = ConfigurationSingleton.GetInstance();
+
+if(ReferenceEquals(cfg, cfg2))
+{
+	Console.WriteLine("Reference are equels");
+}
+
+Console.WriteLine("-------------------------------------------------------------------");
