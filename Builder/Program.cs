1993ebc0bfafc55cb09e421f44e392f872fec21c
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2025-04-01 15:34:46 +0200

    Change Vendor and Vendee name

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -5,8 +5,8 @@ Console.WriteLine("-------------------------------------------------------------
 var invoice = new InvoiceBuilder()
 	.SetInvoiceNumber("24125")
 	.SetDate(new DateTime(2024, 1, 25))
-	.SetVendor("Jan")
-	.SetVendee("Kowalski")
+	.SetVendor("Jan Kowalski")
+	.SetVendee("Ryszard Kowalski")
 	.SetNote("Note")
 	.SetLineItems(new List<string> { "Line Item1", "LineItem2" })
 	.Build();
